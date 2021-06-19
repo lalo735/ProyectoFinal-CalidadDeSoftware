aa26181912d8d525ccc313f0b4e14e91bd2c047e
lalo735 <85080254+lalo735@users.noreply.github.com>
2021-06-19 18:27:34 -0500

    Add files via upload

---
+++ src/main/java/com/fca/calidad/funcionales/pruebas/App.java
@@ -0,0 +1,13 @@
+package com.fca.calidad.funcionales.pruebas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/fca/calidad/funcionales/pruebas/UadyVirtualLoginTest.java
@@ -0,0 +1,89 @@
+package com.fca.calidad.funcionales.pruebas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class UadyVirtualLoginTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+	  //C:\Users\gonza\eclipse-workspace\Driver\chromedriver_win32\chromedriver.exe
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\gonza\\eclipse-workspace\\Driver\\chromedriver_win32\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    driver.get("https://www.google.com/");
+    driver.findElement(By.name("q")).clear();
+    driver.findElement(By.name("q")).sendKeys("uady virtual");
+    driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/a/h3")).click();
+    driver.findElement(By.linkText("Ingresar")).click();
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("a17211344");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("value");
+    driver.findElement(By.id("loginbtn")).click();
+    assertEquals("GONZALO ANTONIO VILLEGAS SULU", driver.findElement(By.xpath("//a[@id='action-menu-toggle-1']/span/span")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
